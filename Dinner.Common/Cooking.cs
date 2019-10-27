c9b4f5684639f725fbb0641aa05889fa590e112e
RedPlus <redplus@redplus.net>
2019-10-28 06:43:41 +0900

    Add project files.

---
+++ Dinner.Common/Cooking.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Dinner.Common
+{
+    public class Cooking
+    {
+        /// <summary>
+        /// 동기 방식의 밥 만들기 메서드 
+        /// </summary>
+        /// <returns>밥</returns>
+        public Rice MakeRice()
+        {
+            Console.WriteLine("밥 생성중...");
+            Thread.Sleep(1001);
+            return new Rice();
+        }
+
+        /// <summary>
+        /// 동기 방식의 국 만들기 메서드
+        /// </summary>
+        /// <returns>국</returns>
+        public Soup MakeSoup()
+        {
+            Console.WriteLine("국 생성중...");
+            Thread.Sleep(1001);
+            return new Soup(); 
+        }
+    }
+
+    public class Rice
+    { 
+        // Pass
+    }
+
+    public class Soup
+    { 
+        // Pass
+    }
+
+    public class Egg
+    { 
+        // Pass
+    }
+}
---
+++ Dinner.Sync/Program.cs
@@ -0,0 +1,28 @@
+﻿using Dinner.Common;
+using System;
+using System.Diagnostics;
+
+namespace Dinner.Sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            //[1] 밥 만들기
+            Rice rice = (new Cooking()).MakeRice(); 
+            Console.WriteLine($"밥 준비 완료 - {rice.GetHashCode()}");
+
+            //[2] 국 만들기
+            Soup soup = (new Cooking()).MakeSoup();
+            Console.WriteLine($"국 준비 완료 - {soup.GetHashCode()}");
+
+            stopwatch.Stop();
+            Console.WriteLine($"시간: {stopwatch.ElapsedMilliseconds}밀리초");
+
+            Console.WriteLine("동기 방식으로 식사 준비 완료");
+        }
+    }
+}
